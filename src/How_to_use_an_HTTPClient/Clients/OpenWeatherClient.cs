6662bf9f189f7b20bef88cb619d07ec13ead7860
Jonuczás Norbert <jonuczas.norbert@gmail.com>
2024-02-27 01:01:59 +0100

    Good_Implementation

--- src/How_to_use_an_HTTPClient/Clients/OpenWeatherClient.cs
+++ src/How_to_use_an_HTTPClient/Clients/OpenWeatherClient.cs
@@ -5,15 +5,22 @@ namespace How_to_use_an_HTTPClient.Clients
     public class OpenWeatherClient : IWeatherClient
     {
         private const string OpenWeatherMapApiKey = "a96e82d421fbd096abf36e26d30944e5";
+        private static readonly HttpClient _httpClient = new(new SocketsHttpHandler
+        {
+            //DNS ISSUE IF TTL EXPIRED
+            PooledConnectionIdleTimeout = TimeSpan.FromMinutes(1),
+        })
+        {
+            BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/")
+
+
+        };
 
         public async Task<WeatherResponse?> GetCurrentWeatherForCity(string city)
         {
-            using var httpClient = new HttpClient
-            {
-                BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/")
-            };
-
-            return await httpClient.GetFromJsonAsync<WeatherResponse?>($"weather?q={city}&appid={OpenWeatherMapApiKey}&units=metric");
+            return
+                await _httpClient.GetFromJsonAsync<WeatherResponse?>(
+                    $"weather?q={city}&appid={OpenWeatherMapApiKey}&units=metric");
         }
     }
 
